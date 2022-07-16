8a033fdafe47bcbdc6b3e28bf737a916d3345ebb
Daniel Fenner <dcfenner@gmail.com>
2022-07-16 01:26:24 -0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div class="appWrapper">
+      <div class="container">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <h5>OW1TRICK</h5>
+      <nav>
+        <a href="https://ow1trick.com">Home</a>
+        <a href="https://ow1trick.com/about">Results</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
